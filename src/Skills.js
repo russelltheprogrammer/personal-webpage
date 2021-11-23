0b139b0f090abdcfc142ea16745c681b2b213a78
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-11-23 14:16:56 -0500

    Work on color transition change

--- src/Skills.js
+++ src/Skills.js
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 const Skills = () => {
 
 const [initialSetting, setInitialSetting] = useState(true);
+const [transitionColors, setTransitionColors] = useState(null);
 
 const handleButtonClick = () => {
-
-return setTimeout(() => {
-     setInitialSetting(false)}, 3000);
+  
+     return setInitialSetting(false) + setTransitionColors("pink");
 }
 
 
      if(initialSetting){
       return ( 
-          <div id="skills">
+          <div id="skills-before">
                <button id="skills-button" onClick={handleButtonClick}>
                <h1 className="skills-heading">SKILLS</h1>
                </button>
@@ -25,17 +25,15 @@ return setTimeout(() => {
     }
     else if(!initialSetting){
      return(
-          <div id="skills">
+          <div id="skills-after" style={{ backgroundColor:transitionColors }}>
               Skills
           </div>
      )
     }
-
-
-
 }
  
 export default Skills;
 
 // click skills button, then fade away, then changed colors and lists skills such as pragamming skills
 // make a scss mixin for skills ID?
+// transition have background-colors change synchronously while chaining promises with a time delay
