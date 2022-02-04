292e3ed3b00ae7f3aa82e916a3d0784b8f356e13
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-04 16:39:05 -0500

    removed

--- src/copyEmailToClipBoard.js
+++
@@ -1,8 +0,0 @@
-const copyEmailToClipBoard = (text) => {
-
-navigator.clipboard.writeText(text);
-
-alert("Russell's E-mail Has Been Copied To The Clipboard.")
-};
-
-export default copyEmailToClipBoard;
