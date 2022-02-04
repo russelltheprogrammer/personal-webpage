7bfd9123217aa22badfdaada115843b493262fe1
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-04 16:38:59 -0500

    redesign

--- src/Skills.js
+++ src/Skills.js
@@ -1,41 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+
 
 const Skills = () => {
 
-const [initialSetting, setInitialSetting] = useState(true);
+     useEffect(() => {
+          Aos.init({duration: 2500});
+      }, []);
+  
 
 
-const handleButtonClick = () => {
-     setInitialSetting(false);
-};
-
-     if(initialSetting){
       return ( 
-           <div id="skills">
-               <div id="skills-before">
-                    <button id="skills-button" onClick={handleButtonClick}>
-                    <h1 className="skills-heading">SKILLS</h1>
-                    </button>
-                    <br/>
-                    <br/>
-                    <i className="far fa-hand-point-up"></i>
-               </div>
+           <div id="skills" data-aos="flip-left">
+               HTML, CSS, JS, REACTJS, SASS, REDUX, BOOTSTRAP, GIT/GITHUB, VSC, CLI
           </div>
           );
-    }
-    else if(!initialSetting){
-     return(
-          <div id="skills">
-               <div id="skills-after">
-               HTML, CSS, JS, REACTJS, SASS, REDUX, BOOTSTRAP, GIT/GITHUB, VSC, CLI
-               </div>
-          </div>
-     )
-    }
+    
 }
  
 export default Skills;
-
-// click skills button, then fade away, then changed colors and lists skills such as pragamming skills
-// make a scss mixin for skills ID?
-// transition have background-colors change synchronously while chaining promises with a time delay
