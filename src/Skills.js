b7e149ba47044ce203d0e97497da262b1dc7fa9f
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-14 17:23:57 -0500

    work on skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -14,7 +16,12 @@ const Skills = () => {
 
       return ( 
            <div id="skills">
-               HTML, CSS, JS, REACTJS, SASS, REDUX, BOOTSTRAP, GIT/GITHUB, VSC, CLI
+                <div id="skills-container">
+                    <h1>~Russell's Skills~</h1>
+                    <div>
+                    <FontAwesomeIcon icon={faRocket} /> HTML5    CSS3, JavaScript ES6, ReactJS, REDUX, BOOTSTRAP, SASS, GIT/GITHUB
+                    </div>
+               </div>
           </div>
           );
     
