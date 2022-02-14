8ce76910c04d3c8fdb53de5cc4260c5a21620f70
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-14 17:33:10 -0500

    fontawesome icons

--- src/Skills.js
+++ src/Skills.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRocket } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5, faCss3, faJs, faReact, faBootstrap, faSass, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -19,7 +19,10 @@ const Skills = () => {
                 <div id="skills-container">
                     <h1>~Russell's Skills~</h1>
                     <div>
-                    <FontAwesomeIcon icon={faRocket} /> HTML5    CSS3, JavaScript ES6, ReactJS, REDUX, BOOTSTRAP, SASS, GIT/GITHUB
+                    <FontAwesomeIcon icon={faHtml5} />&nbsp;HTML5&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faCss3} />&nbsp;CSS3&nbsp;&nbsp;&nbsp;
+                    <FontAwesomeIcon icon={faJs} />&nbsp;JavaScript (ES6)&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faReact} />&nbsp;ReactJS&nbsp;&nbsp;&nbsp;
+                    <FontAwesomeIcon icon={faBootstrap} />&nbsp;Bootstrap&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faSass} />&nbsp;SASS&nbsp;&nbsp;&nbsp;
+                    <FontAwesomeIcon icon={faGithub} />&nbsp;GIT/GITHUB&nbsp;&nbsp;&nbsp;REDUX
                     </div>
                </div>
           </div>
