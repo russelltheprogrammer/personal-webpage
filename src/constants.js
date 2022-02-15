e15761dc3e84e3076b540c21ef267c71f360901b
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-15 18:05:11 -0500

    add constants file

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const colors = ["white", "red", "orange", "yellow", "green", "darkBlue", "#16063A", "violet"];
+
+const flexItems = [ 
+    { id: 0, title: "JavaScript Calculator", url: "https://russelltheprogrammer.github.io/javascript-calculator/" }, 
+    { id: 1, title: "MonteithTaxCPA.com", url: "http://monteithtaxcpa.com" }, 
+    { id: 2, title: "Spacestagram", url: "https://russelltheprogrammer.github.io/spacestagram/" }, 
+    { id: 3, title: "25-5-Clock", url: "https://russelltheprogrammer.github.io/25-5-clock/" }, 
+    { id: 4, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/"}, 
+    { id: 5, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/" },
+    { id: 6, title: "Tax Projection Application", url: "https://github.com/russelltheprogrammer"}
+ ];
+
+export {colors, flexItems};
