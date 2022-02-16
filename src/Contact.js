94bc9d1d51cb07afff076955da5163ab78b9c499
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-16 17:12:41 -0500

    update contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,44 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+
 const Contact = () => {
     return ( 
         <div id="contact">
-                <h1>~Contact Information~</h1>
-                    <div id="contact-grid">
-                        <a  href="https://forum.freecodecamp.org/u/rundownx3/summary"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="contact-item"
-                        id="profile-link"
-                        >
-                        <p>FCC Profile
-                        </p>
-                        </a>
-                        <a  href="https://github.com/russelltheprogrammer"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="contact-item"
-                        id="profile-link"
-                        >
-                        <p>GitHub Profile
-                        </p>
-                        </a>
-                        <a  href="mailto: russelltheprogrammer@gmail.com"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="contact-item"
-                        id="profile-link"
-                        >
-                        <p>Send me an e-mail
-                        </p>
-                        </a>
-                    <a  href="tel:+1234567890"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="contact-item"
-                        id="profile-link"
-                        >
-                        <p>Call me maybe (Not a real phone number)
-                        </p>
-                        </a>
+                <h1>~Contact~</h1>
+                    <div id="contact-container">
+                        <p>Russell</p>
+                        <p><FontAwesomeIcon icon={faPhoneSquare} /> (347) 508 3407</p>
+                        <p><FontAwesomeIcon icon={faPaperPlane} /> russelltheprogrammer@gmail.com</p>
                     </div>
             </div>
      );
