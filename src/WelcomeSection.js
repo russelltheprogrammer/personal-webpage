314868b1a9682d762b184a80c35eff4d17559617
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-02-16 17:32:33 -0500

    update image url

--- src/WelcomeSection.js
+++ src/WelcomeSection.js
@@ -12,7 +12,7 @@ const WelcomeSection = () => {
         <div id="welcome-section" data-aos="zoom-in-down">
                 <h1>I am Russell Monteith</h1>
                     <p>A Front End Developer on the journey to becoming a Full Stack Developer. Scroll down to find out more...</p>
-                    <img className="welcome-headshot" src="/headshot.jpg" alt="Headshot"/>
+                    <img className="welcome-headshot" src="../headshot.jpg" alt="Headshot"/>
         </div>
      );
 }
