c98e0795fe0e4dfa7e34d61a23424b5ffda50e8c
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-03 09:28:19 -0500

    update link for tax projection app

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ const flexItems = [
     { id: 3, title: "25-5-Clock", url: "https://russelltheprogrammer.github.io/25-5-clock/" }, 
     { id: 4, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/"}, 
     { id: 5, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/" },
-    { id: 6, title: "Tax Projection Application (In Process)", url: "https://github.com/russelltheprogrammer"}
+    { id: 6, title: "Tax Projection Application (In Process)", url: "https://github.com/russelltheprogrammer/tax-projection-individual"}
  ];
 
 export {colors, flexItems};
