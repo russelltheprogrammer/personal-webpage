8036e01315c3f23f19d274e75343152b0b48047e
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-19 13:41:20 -0400

    change links order and add notes

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 const colors = ["white", "red", "orange", "yellow", "green", "darkBlue", "#16063A", "violet"];
 
 const flexItems = [ 
-    { id: 0, title: "JavaScript Calculator", url: "https://russelltheprogrammer.github.io/javascript-calculator/" }, 
+    { id: 0, title: "Tax Projection Application (In Process)", url: "https://github.com/russelltheprogrammer/tax-projection-individual"},
     { id: 1, title: "Monteithtaxcpa.com", url: "http://monteithtaxcpa.com" }, 
-    { id: 2, title: "Spacestagram", url: "https://russelltheprogrammer.github.io/spacestagram/" }, 
-    { id: 3, title: "25-5-Clock", url: "https://russelltheprogrammer.github.io/25-5-clock/" }, 
-    { id: 4, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/"}, 
-    { id: 5, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/" },
-    { id: 6, title: "Tax Projection Application (In Process)", url: "https://github.com/russelltheprogrammer/tax-projection-individual"}
+    { id: 2, title: "JavaScript Calculator", url: "https://russelltheprogrammer.github.io/javascript-calculator/" }, 
+    { id: 3, title: "Spacestagram", url: "https://russelltheprogrammer.github.io/spacestagram/" }, 
+    { id: 4, title: "25-5-Clock", url: "https://russelltheprogrammer.github.io/25-5-clock/" }, 
+    { id: 5, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/"}, 
+    { id: 6, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/" }
  ];
 
 export {colors, flexItems};
