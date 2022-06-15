02af611f76bb0005bc76ad4beea2278501c89a69
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-15 12:51:09 -0400

    change design

--- src/WelcomeSection.js
+++ src/WelcomeSection.js
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
 import headshot from "./images/headshot.jpg";
-import "aos/dist/aos.css";
+import * as React from 'react';
+import {
+    Card
+} from "@mui/material";
+
 
 const WelcomeSection = () => {
 
-    useEffect(() => {
-        Aos.init({duration: 2500});
-    }, []);
-
-    return ( 
-        <div id="welcome-section" data-aos="zoom-in-down">
+    return (
+        <div id="welcome-section">
+            <Card
+              sx={{
+                marginTop: '6%',
+                padding: "1%",
+                width: "70%",
+                color: 'white',
+                textAlign: "center",
+                backgroundColor: "#8A7BAF",
+              }}
+            >
                 <h1>I am Russell Monteith</h1>
                     <p>A Front End Developer on the journey to becoming a Full Stack Developer. Scroll down to find out more...</p>
                     <img className="welcome-headshot" src={headshot} alt="Headshot"/>
+            </Card>
         </div>
      );
 }
