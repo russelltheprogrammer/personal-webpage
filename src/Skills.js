975d42a5e6d580d185758545184b80a3a39faf62
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-20 17:43:08 -0400

    start redesign

--- src/Skills.js
+++ src/Skills.js
@@ -1,49 +1,85 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import SkillsList from './SkillsList.js';
-import { colors } from './constants.js';
+import { skillItems }from './constants.js';
+// import { colors } from './constants.js';
+import {
+     Card,
+     Container,
+     Grid,
+     Paper,
+ } from "@mui/material";
 
 
 
 const Skills = () => {
 
-const [ fontColor, setFontColor ] = useState("white");
-const [ fontColorNumber, setFontColorNumber ] = useState(0);
-const [isDesktop, setIsDesktop] = useState(window.innerWidth > 770);
+// const [ fontColor, setFontColor ] = useState("white");
+// const [ fontColorNumber, setFontColorNumber ] = useState(0);
+// const [isDesktop, setIsDesktop] = useState(window.innerWidth > 770);
 
-useEffect(() => {
-     let number = fontColorNumber;
-     let timer = setTimeout(() => {
-          if(number === 7){
-               setFontColorNumber(0);
-               setFontColor(colors[0]);
-          }
-          else{
-               setFontColorNumber(number + 1);
-               setFontColor(colors[number + 1]);
-          }
-     }, 2000);
-     return () => clearTimeout(timer);
-});
+// useEffect(() => {
+//      let number = fontColorNumber;
+//      let timer = setTimeout(() => {
+//           if(number === 7){
+//                setFontColorNumber(0);
+//                setFontColor(colors[0]);
+//           }
+//           else{
+//                setFontColorNumber(number + 1);
+//                setFontColor(colors[number + 1]);
+//           }
+//      }, 2000);
+//      return () => clearTimeout(timer);
+// });
 
 
-useEffect(() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia)
-});
+// useEffect(() => {
+//     window.addEventListener("resize", updateMedia);
+//     return () => window.removeEventListener("resize", updateMedia)
+// });
 
-const updateMedia = () => {
-    setIsDesktop(window.innerWidth > 890)
-};
+// const updateMedia = () => {
+//     setIsDesktop(window.innerWidth > 890)
+// };
 
 
       return ( 
-           <div id="skills">
-                <div id="skills-container">
-                    <h1>~Skills~</h1>
-                    <div style={{color: fontColor }}>
-                         {isDesktop ? <SkillsList props={true} /> : <SkillsList props={false} />}
+          <div>
+               <Container
+               sx={{
+                    width: "100%",
+                    color: "white",
+                    textAlign: "center",
+                    paddingTop: "1rem",
+                    paddingBottom: "5rem",
+               }}>
+                    <div id="skills-header">
+                         <h1 style={{ fontSize: "4rem", fontStyle: "italic", paddingBottom:  "1rem" }}>~Skills~</h1>
                     </div>
-               </div>
+                         <Card
+                         sx={{
+                              padding: "3rem 0",
+                              width: "80%",
+                              marginLeft: "10%",
+                              fontSize: "3rem",
+                              color: "black",
+                         }}>
+                              <Grid container spacing={1} alignItems="center" justifyContent="center">
+                                   {skillItems.map((item) => (
+                                       <Grid item key={item.id} xs={12} sm={6} md={4} lg={4} xl={4}>
+                                        <Paper>{item.title}</Paper>
+                                       </Grid>
+                                   ))}
+          
+                               {/* <div style={{color: "blue" }}>
+                                    <SkillsList props={true} />
+                                   {isDesktop ? <SkillsList props={true} /> : <SkillsList props={false} />}
+                              </div> */}
+
+                              </Grid>
+                         </Card>
+                         
+               </Container>
           </div>
           );
 }
