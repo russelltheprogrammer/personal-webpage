d216f4092c6ac77af20e8ca1fc45bcadb2174a96
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-20 17:42:46 -0400

    edit

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ import quote from "./images/quote.jpg";
 
 const colors = ["white", "red", "orange", "yellow", "green", "darkBlue", "#16063A", "violet"];
 
-const flexItems = [ 
+const projectItems = [ 
     { id: 0, title: "Tax Projection Application", url: "https://russelltheprogrammer.github.io/tax-projection-individual/", img: taxes, alt: "Taxes" },
     { id: 1, title: "Monteithtaxcpa.com", url: "http://monteithtaxcpa.com", img: cpaLogo, alt: "CPA Website" }, 
     { id: 2, title: "JavaScript Calculator", url: "https://russelltheprogrammer.github.io/javascript-calculator/", img: calculator, alt: "Calculator" }, 
@@ -20,4 +20,18 @@ const flexItems = [
     { id: 6, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/", img: quote, alt: "Quote" }
  ];
 
-export {colors, flexItems};
+ const skillItems = [
+    { id: 0, title: "HTML5"},
+    { id: 1, title: "CSS3" },
+    { id: 2, title: "JavaScript(ES6)" },
+    { id: 3, title: "TypeScript" },
+    { id: 4, title: "ReactJS" },
+    { id: 5, title: "Material UI" },
+    { id: 6, title: "Bootstrap" },
+    { id: 7, title: "SASS" },
+    { id: 8, title: "Git/GitHub" },
+    { id: 9, title: "REDUX" },
+    { id: 10, title: "Heroku" }
+ ]
+
+export {colors, projectItems, skillItems};
