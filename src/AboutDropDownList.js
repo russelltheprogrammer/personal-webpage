457764f82fd63a3aaafa627bc4a851f7c41cf6ac
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-29 12:50:02 -0400

    redesign

--- src/AboutDropDownList.js
+++ src/AboutDropDownList.js
@@ -1,6 +1,6 @@
 const AboutDropDownList = ({props}) => {
 
-    if(props.description === "Who is Russell?" && props.isActive) {
+    if(props.description === "WHO AM I" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
                     <p>I live in New York City and I have been doing web development since July 2020 when I created my first Javascript variable.
@@ -11,7 +11,7 @@ const AboutDropDownList = ({props}) => {
             </div>
         )
     }
-    else if(props.description === "What does Russell do right now?" && props.isActive) {
+    else if(props.description === "WHAT I DO RIGHT NOW" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
                 <p>Currently, I work part time in accounting and spend the rest of my time focused on my goal of becoming a full time 
@@ -21,12 +21,12 @@ const AboutDropDownList = ({props}) => {
             </div>
         )
     }
-    else if(props.description === "What Russell used to do?" && props.isActive) {
+    else if(props.description === "WHAT I USED TO DO" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
                 <p> I used to be a full time accountant but I always knew I would not work as an accountant forever. I did not discover my
                     passion for web development until later in life and I am sure glad I did because I am very happy on this new journey.
-                    As an accountant, I have over 8 years of experience working in tax and I am a Certified Public Accountant 
+                    As an accountant, I have over 9 years of experience working in tax and I am a Certified Public Accountant 
                     in the State of New York. I still manage a side business to this day. Check out my website at&nbsp;
                     <a href="http://www.monteithtaxcpa.com/"
                     target="_blank"
@@ -35,7 +35,7 @@ const AboutDropDownList = ({props}) => {
             </div>
         )
     }
-    else if(props.description === "Education" && props.isActive) {
+    else if(props.description === "EDUCATION" && props.isActive) {
         return (
             <div>
                 <ul className="about-drop-down-word-list">
@@ -43,17 +43,17 @@ const AboutDropDownList = ({props}) => {
                     <li>freeCodeCamp Javascript Algorithms and Data Structures Certification</li>
                     <li>freeCodeCamp Front End Libraries Certification</li>
                     <li>Graduated from Binghamton University with a Bachelor of Science in Accounting</li>
-                    <li>Graduated from Binghamton University with a Masters in Accounting</li>
+                    <li>Graduated from Binghamton University with a Master of Science in Accounting</li>
                 </ul>
             </div>
         )
     }
-    else if(props.description === "Outside Interests/Hobbies" && props.isActive) {
+    else if(props.description === "OUTSIDE INTERESTS" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
                 <p>Some people might find it hard to believe but I do not program 24/7, 365 days a year. Of course, I enjoy it but other times
                     you can find me partaking in other activities such as watching football/Formula1, playing video games/board games, 
-                    exercising, listening to synthwave music, or simply relaxing in space.
+                    exercising, being outdoors, listening to synthwave music, or simply relaxing in space.
                 </p>
             </div>
         )
