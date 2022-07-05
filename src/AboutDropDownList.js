05b217198fb674f53b77566caf32586788e0b834
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-05 15:13:52 -0400

    changed blurbs

--- src/AboutDropDownList.js
+++ src/AboutDropDownList.js
@@ -3,10 +3,10 @@ const AboutDropDownList = ({props}) => {
     if(props.description === "WHO AM I" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
-                    <p>I live in New York City and I have been doing web development since July 2020 when I created my first Javascript variable.
-                    This was a big moment for someone trying to find their calling in life. Ever since then, I have been hooked on programming.
-                    I am now looking to join a team that shares my similar excitement for web development. As a profession that allows for forever learning
-                    and daily problem solving, I can't help but flock to it.
+                    <p>I am self-taught web developer based out of New York City. My journey began in July 2020 when I created my first Javascript variable.
+                    <br/><code>var x = "hello world"</code><br/>
+                    Ever since then, I have been hooked on programming and continue to refine my skills on a daily basis.
+                    I am currently looking to join a team that shares my same passion.
                     </p>
             </div>
         )
@@ -14,9 +14,9 @@ const AboutDropDownList = ({props}) => {
     else if(props.description === "WHAT I DO RIGHT NOW" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
-                <p>Currently, I work part time in accounting and spend the rest of my time focused on my goal of becoming a full time 
-                    web developer. Some of the ways I develop my programming skills are by building personal projects, learning new concepts, best practices 
-                    and technologies, and practicing my algorithm skills.
+                <p>Currently, I work part time in tax accounting and spend the rest of my time focused on my goal of becoming a full time 
+                    web developer. Some of the ways I develop my programming skills are by building personal projects, learning new concepts/best practices/technologies, 
+                    reading about computer science fundamentals, and practicing my algorithm skills.
                 </p>
             </div>
         )
@@ -24,8 +24,8 @@ const AboutDropDownList = ({props}) => {
     else if(props.description === "WHAT I USED TO DO" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
-                <p> I used to be a full time accountant but I always knew I would not work as an accountant forever. I did not discover my
-                    passion for web development until later in life and I am sure glad I did because I am very happy on this new journey.
+                <p> I used to work full time as a tax accountant and stumbled upon web development randomly during the Pandemic after conversing with a fellow web developer.
+                    For fun, I started programming, then later realized I could teach myself to get a full time position.<br/><br/>
                     As an accountant, I have over 9 years of experience working in tax and I am a Certified Public Accountant 
                     in the State of New York. I still manage a side business to this day. Check out my website at&nbsp;
                     <a href="http://www.monteithtaxcpa.com/"
@@ -51,7 +51,7 @@ const AboutDropDownList = ({props}) => {
     else if(props.description === "OUTSIDE INTERESTS" && props.isActive) {
         return (
             <div className="about-drop-down-word-blurb">
-                <p>Some people might find it hard to believe but I do not program 24/7, 365 days a year. Of course, I enjoy it but other times
+                <p>It might be hard to believe but I do not program 24/7, 365 days a year. Of course, I enjoy it but other times
                     you can find me partaking in other activities such as watching football/Formula1, playing video games/board games, 
                     exercising, being outdoors, listening to synthwave music, or simply relaxing in space.
                 </p>
