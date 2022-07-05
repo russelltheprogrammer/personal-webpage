b3a5a9ba0659f4b63d727228ac2ba929dc1e4826
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-05 15:56:21 -0400

    redesign

--- src/Contact.js
+++ src/Contact.js
@@ -2,13 +2,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
+
+const sendEmail = () => {
+    window.location = "mailto:russelltheprogrammer@gmail.com";
+}
+
+const sendPhoneNumber = () => {
+    window.location= "tel:347-508-3407";
+}
+
     return ( 
         <div id="contact">
             <h1>~CONTACT~</h1>
                 <div id="contact-container">
                     <p className='contact-name'>RUSSELL</p>
-                    <p><FontAwesomeIcon icon={faPhoneSquare} /> (347) 508 3407</p>
-                    <p className="contact-email"><FontAwesomeIcon icon={faPaperPlane} /> russelltheprogrammer@gmail.com</p>
+                    <p onClick={sendPhoneNumber} className='contact-phone-number'><FontAwesomeIcon icon={faPhoneSquare} /> (347) 508 3407</p>
+                    <p onClick={sendEmail} className="contact-email"><FontAwesomeIcon icon={faPaperPlane} /> russelltheprogrammer@gmail.com</p>
                 </div>
         </div>
      );
