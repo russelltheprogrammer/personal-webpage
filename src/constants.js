9e4d5de04cea0b84388d8ee7f6dc798815b5088d
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-11-08 14:51:56 -0500

    remove project

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,6 @@ import calculator from "./images/calculator.jpg";
 import space from "./images/space.jpg";
 import stopWatch from "./images/stopWatch.jpg";
 import drumMachine from "./images/drumMachine.jpg";
-import quote from "./images/quote.jpg";
 
 // all images are open source except CPA Logo which is owned by Russell Monteith CPA PLLC
 
@@ -14,8 +13,7 @@ const projectItems = [
     { id: 2, title: "JavaScript Calculator", url: "https://russelltheprogrammer.github.io/javascript-calculator/", img: calculator, alt: "Calculator" }, 
     { id: 3, title: "Spacestagram", url: "https://russelltheprogrammer.github.io/spacestagram/", img: space, alt: "Space" }, 
     { id: 4, title: "25-5-Clock", url: "https://russelltheprogrammer.github.io/25-5-clock/", img: stopWatch, alt: "Stop Watch" }, 
-    { id: 5, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/", img: drumMachine, alt: "Drum Machine" }, 
-    { id: 6, title: "Random Quote Machine", url: "https://russelltheprogrammer.github.io/random-quote-machine/", img: quote, alt: "Quote" }
+    { id: 5, title: "Drum Machine", url: "https://russelltheprogrammer.github.io/drum-machine/", img: drumMachine, alt: "Drum Machine" }
  ];
 
  const skillItems = [
